d688435e62aa86c6c9ff12ccd8ed1f5c3ba686ee
ImL1s <aa22396584@gmail.com>
2019-04-10 16:57:16 +0800

    add: use redis string&hash

--- src/main/java/com/iml1s/spring/redis/controller/RedisController.java
+++ src/main/java/com/iml1s/spring/redis/controller/RedisController.java
@@ -2,17 +2,57 @@ package com.iml1s.spring.redis.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/redis")
-public class RedisTemplate {
+public class RedisController {
 
     @Autowired
     private RedisTemplate redisTemplate;
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping("/stringAndHash")
+    @ResponseBody
+    public Map<String, Object> testStringAndHash() {
+        redisTemplate.opsForValue().set("key1", "value1");
+        redisTemplate.opsForValue().set("int_key", "1");
+
+        // 非數值,會異常(RedisTemplate使用JDK序列化器)
+//        redisTemplate.opsForValue().increment("int_key");
+
+        stringRedisTemplate.opsForValue().set("int_key_s", "1");
+        stringRedisTemplate.opsForValue().increment("int_key_s");
+        stringRedisTemplate.opsForValue().decrement("int_key_s");
+
+        // 使用底層Jedis連接
+//        Jedis jedis = (Jedis) stringRedisTemplate.getConnectionFactory().getConnection().getNativeConnection();
+//        jedis.decr("int_key_s");
+
+        Map<String, String> hash = new HashMap<>();
+        hash.put("field1", "value1");
+        hash.put("field2", "value2");
+        stringRedisTemplate.opsForHash().putAll("hash", hash);
+        stringRedisTemplate.opsForHash().put("hash", "field3", "value3");
+
+        // 綁定hash的key
+        BoundHashOperations operations = stringRedisTemplate.boundHashOps("hash");
+        operations.delete("field1", "field2");
+        operations.put("field4", "value4");
+
+        HashMap map = new HashMap<String, Object>();
+        map.put("success", true);
+        return map;
+    }
 }
